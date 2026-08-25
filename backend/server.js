16c9e15f1eaee6e2328ab133f6515397a697ee00
Aayush Gurung <123372782+AayushGurung22@users.noreply.github.com>
2026-08-25 12:58:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,13 @@ const cors = require('cors')
 dotenv.config()
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGO_URI;
 const client = new MongoClient(url);
 
 // Database name
 const dbName = 'passop';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyparser.json())
 app.use(cors())
 
